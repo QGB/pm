32f894c1d0282d0a92a264ab9edfd66c07a268cd
QGB <qgbcs1@gmail.com>
2015-11-05 18:01:04 +0800

    othello  getround

--- othello.py
+++ othello.py
@@ -4,6 +4,7 @@ from sys import *
 from qgb import U,T
 
 class gui:
+#white 0 2, black 1 3,5 not,9 null
 	def __im(s,a):
 		if(a<3):
 			raise Exception("a<3")
@@ -12,11 +13,20 @@ class gui:
 	def onclick(s,event):
 		b=event.widget
 		btv=b['textvariable']
+		btv=str(btv).split(',')
+		i=int(btv[0])
+		j=int(btv[1])
+		b['bg']='green'
+		if(i==0):
+			b['bg']='black'
+		if(j==0):
+			b['bg']='white'
 		
-		U.p(btv)
 		# j=b['textvariable']['j']
-		#U.p(66)
-		U.msgbox()
+		# print b['bg'],type(b['bg'])
+		U.pln(i,j)
+		U.pln(s.f.geometry())
+	#U.msgbox(i,c,btv)
 		
 	def setBtn(s,im=8):
 		s.__im(im)
@@ -28,15 +38,83 @@ class gui:
 				s.yb[i].append(Button(s.f,))#text=str(i)+','+str(j) 
 				b=s.yb[i][j]
 				b.place(relx=j*w,rely=i*w,relwidth=w,relheight=w)
-				b['textvariable']={'i':i,'j':j}
-				print b['textvariable']
+				b['textvariable']=str(i)+','+str(j)						
 				b.bind('<Button>',s.onclick)
-		#print s.yb
+				
+		s.f.geometry('793x724+1+-24')
+		# print ia,w,w*64
 	def __init__(s,im=8):
-		s.__im(im)
 		s.f=Tk()
+		s.setBtn(im)
 		Thread(target=s.f.mainloop).start()
-g=gui()
 
-print g.im
-g.setBtn(4)
+class da:
+	def __init__(s,im):
+		s.im=im
+		s.im1=im-1
+		i2=(s.im/2)
+		s.y=[]
+		for i in range(s.im):
+			s.y.append([])
+			for j in range(s.im):
+				s.y[i].append(5)#text=str(i)+','+str(j) 
+			s.y[i].append(9)#list[-1] is last
+			
+		
+		s.y[i2-1][i2-1]=0
+		s.y[i2][i2-1]=1
+		s.y[i2-1][i2]=1
+		s.y[i2][i2]=0
+		
+
+		s.y.append([9 for i in range(s.im+1)])
+		
+	def init(s):
+		pass
+	#im
+	def getround(s,i,j):
+		r=[]
+		ri=9
+		try:r.append(s.y[i-1][j])
+		except:r.append(ri)
+		try:r.append(s.y[i-1][j+1])
+		except:r.append(ri)
+		try:r.append(s.y[i][j+1])
+		except:r.append(ri)
+		try:r.append(s.y[i+1][j+1])
+		except:r.append(ri)
+		try:r.append(s.y[i+1][j])
+		except:r.append(ri)
+		try:r.append(s.y[i+1][j-1])
+		except:r.append(ri)
+		try:r.append(s.y[i][j-1])
+		except:r.append(ri)
+		try:r.append(s.y[i-1][j-1])
+		except:r.append(ri)
+		return r
+	def near(s,ac):
+		for i in range(s.im):
+			for j in range(s.im):
+				c=s.y[i][j]
+				if(c==ac):
+					for k in getround(i,j):
+						
+	def aw(s):
+		pass
+	
+	def p(s):
+		for i in range(len(s.y)):
+			for j in range(len(s.y[i])):
+				U.p(s.y[i][j],' ')
+			print
+
+
+d=da(22)
+# #
+d.aw()
+d.p()
+# print d.getround(0,0)
+# U.msgbox()
+#g=gui(33)
+# #print g.im
+# g.setBtn(5)
--- tkBtn.py
+++ tkBtn.py
@@ -1,2 +1,64 @@
 d=['activebackground', 'activeforeground', 'anchor', 'background', 'bd', 'bg', 'bitmap', 'borderwidth', 'command', 'compound', 'cursor', 'default', 'disabledforeground', 'fg', 'font', 'foreground', 'height', 'highlightbackground', 'highlightcolor', 'highlightthickness', 'image', 'justify', 'overrelief', 'padx', 'pady', 'relief', 'repeatdelay', 'repeatinterval', 'state', 'takefocus', 'text', 'textvariable', 'underline', 'width', 'wraplength']
 print d
+'''color
+white <type 'str'>
+(66,)
+black <type 'str'>
+(66,)
+black <type 'str'>
+(66,)
+SystemButtonFace <type 'str'>
+(66,)
+SystemButtonFace <type 'str'>
+(66,)
+SystemButtonFace <type 'str'>
+(66,)
+SystemButtonFace <type 'str'>
+(66,)
+white <type 'str'>
+(66,)
+white <type 'str'>
+(66,)
+white <type 'str'>
+(66,)
+black <type 'str'>
+(66,)
+'''
+#############################################
+'''
+activebackground SystemButtonFace <type 'str'>
+activeforeground SystemButtonText <type 'str'>
+anchor center <type 'str'>
+background SystemButtonFace <type 'str'>
+bd 2 <type '_tkinter.Tcl_Obj'>
+bg SystemButtonFace <type 'str'>
+bitmap  <type 'str'>
+borderwidth 2 <type '_tkinter.Tcl_Obj'>
+command  <type 'str'>
+compound none <type 'str'>
+cursor  <type 'str'>
+default disabled <type 'str'>
+disabledforeground SystemDisabledText <type 'str'>
+fg SystemButtonText <type 'str'>
+font TkDefaultFont <type 'str'>
+foreground SystemButtonText <type 'str'>
+height 0 <type 'int'>
+highlightbackground SystemButtonFace <type 'str'>
+highlightcolor SystemWindowFrame <type 'str'>
+highlightthickness 1 <type '_tkinter.Tcl_Obj'>
+image  <type 'str'>
+justify center <type 'str'>
+overrelief  <type 'str'>
+padx 1 <type '_tkinter.Tcl_Obj'>
+pady 1 <type '_tkinter.Tcl_Obj'>
+relief raised <type 'str'>
+repeatdelay 0 <type 'int'>
+repeatinterval 0 <type 'int'>
+state normal <type 'str'>
+takefocus  <type 'str'>
+text  <type 'str'>
+textvariable {'i': 0, 'j': 3} <type '_tkinter.Tcl_Obj'>
+underline -1 <type 'int'>
+width 0 <type 'int'>
+wraplength 0 <type '_tkinter.Tcl_Obj'>
+'''
