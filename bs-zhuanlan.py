06a5a335dcadaf6926a9cbff3d1985b9e2a25e4f
QGB <qgbcs1@gmail.com>
2015-11-20 23:41:48 +0800

    rm

--- bs-zhuanlan.py
+++ bs-zhuanlan.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as bs
 import urllib2
 from qgb import U,T
 
-# U.helphtml(U.x)
+U.helphtml(open(r"D:\test\zys.html"))
 # print dir('645'),'645'.__len__
 # help(U.x)
 
@@ -14,32 +14,46 @@ from qgb import U,T
 
 # U.eval('''dicthtml('U.globals.html',globals())''')
 # exec('''U.dicthtml('this.exec.globals.html',globals())''')
-# exit()
+exit()
 
 s=open(r"D:\test\zys.html").read()
 sp=bs(s,"html.parser" ) 
 
 #class="in-reply-to ng-hide"report(comment)
-for j in sp.find_all(attrs={'class':"in-reply-to ng-hide"}):
-	j.decompose()  
+# for j in sp.find_all(attrs={'class':"in-reply-to ng-hide"}):
+	# j.decompose()  
 for j in sp.find_all(attrs={'ng-if':"canReply(comment)"}):
 	j.decompose()  
 for j in sp.find_all(attrs={'ng-click':"report(comment)"}):
 	j.decompose()  
 	
+open(r"D:\test\zys0.html",'wb').buffer.write(sp)
+exit()
 ls=[]
-for i in sp.find_all(attrs={"class":"comment-body"}):
+
+for i in sp.find_all(attrs={"class":"comment-body"},limit=344):
+	name,href,rto=None,None,None
 	# print i.text.encode('gbk')
 	for j in i.find_all():
-		href=j.get('href');href=str(href)
-		if(href.find('http://www.zhihu.com/people/')!=-1):
-			sn= j.text.encode('gbk')
-			if href not in ls:
-				ls.append(href)
+		# print j.get('class')
+		if(str(j.get('class')).find(u'in-reply-to')>0):
+			rto=j.find(attrs={"class":"ng-binding"})
+			if(len(rto.text)<1):rto=None
+			continue
+			
+		if(str(j.get('href')).find('http://www.zhihu.com/people/')!=-1):
+			href=j.get('href');href=str(href)
+			name=j.text.encode('gbk')
+			continue
+			# print name
+			# if href not in ls:ls.append(href)
 				
 				
-				
+	print name,href,rto	
+	ls.append((name,href,rto))
 for j in ls :
 	print j
+	
+print len(ls)
 	# U.msgbox()
 	# U.helphtml(i)
--- gitpm.cmd
+++ gitpm.cmd
@@ -1,9 +1,11 @@
 cd qgb
+rm *.pyc
 git add -A
 git commit -m %1%
 git push qpsu master
 
 cd ..
+rm *.pyc
 git add -A
 git commit -m %1%
 git push pm master
